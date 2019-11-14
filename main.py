adc1bd01faf4a2b8c5691c27ffeb3087dbcf4cec
Neekon Saadat <neekonsu@gmail.com>
2019-11-14 13:53:37 -0800

    some changes?

---
+++ main.py
@@ -0,0 +1,45 @@
+from pylsl import StreamInfo, StreamOutlet
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+
+# first create a new stream info (here we set the name to BioSemi,
+# the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
+# last value would be the serial number of the device or some other more or
+# less locally unique identifier for the stream as far as available (you
+# could also omit it but interrupted connections wouldn't auto-recover).
+info = StreamInfo('BioSemi', 'EEG', 8, 100, 'float32', 'myuid34234')
+
+# next make an outlet
+outlet = StreamOutlet(info)
+
+async def push(OUTLET, frame):
+    OUTLET.push_sample(frame)
+    await asyncio.sleep(1)
+
+print("now sending data...")
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+ 
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+    push(outlet, image)
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
