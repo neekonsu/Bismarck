abadcf1f019c3b4805dc9dd0d0b5c73de155878d
Neekon Saadat <neekonsu@gmail.com>
2019-11-23 19:29:15 -0800

    adsaads

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,5 @@
-from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time, os, datetime
-picamera = PiCamera()
 # set working directory and get current datetime
 wd = "/var/bismarck/"
 current_date = datetime.datetime.now().strftime("%c").replace(" ", "_")
@@ -14,13 +12,13 @@ except OSError:
 # allow the camera to warmup
 time.sleep(0.2)
 # Recording sequence
-with picamera as camera:
+with PiCamera() as camera:
     camera.resolution = (1280, 720)
     camera.framerate = 32
     camera.start_preview()
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    camera.start_recording('%s.h264', current_date)
+    camera.start_recording(wd + '%s.h264', current_date)
     start = dt.datetime.now()
     while (dt.datetime.now() - start).seconds < 3600:
         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
