4589dd6fa8c6b97771a1826eb7639d945d9423ad
Neekon Saadat <neekonsu@gmail.com>
2019-11-26 21:26:41 -0800

    +(Git Support) ~(Confirmed no file-overwrite)

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,17 @@
 import picamera
 import time, os
 import datetime as dt
+import atexit
+from git import Repo
+# Define procedure for program exit -> save onExit
+def onExit():
+    index.commit("+(Footage)")
+atexit.register(onExit)
 # set working directory and get current datetime
 wd = "/var/bismarck/"
 current_date = dt.datetime.now().strftime("%c").replace(" ", "_")
+repo = Repo.init(wd).git
+index = Repo.init(wd).index
 # initialize list of existing captures (xor) create working directory
 try:
 	os.mkdir(wd)
@@ -20,9 +28,12 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    camera.start_recording(wd + current_date +".h264")
+    tmp_filename = wd + current_date +".h264"
+    camera.start_recording(tmp_filename)
     start = dt.datetime.now()
     while (dt.datetime.now() - start).seconds < 3600:
         camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         camera.wait_recording(0.2)
     camera.stop_recording()
+    repo.add(tmp_filename)
+index.commit("+(Footage)")
